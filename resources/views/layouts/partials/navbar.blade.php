a6fdcfea73a14505e363fbae63874373944ba591
George Vincent Peña <gvbpena@gmail.com>
2024-03-05 12:23:07 +0800

    ara oh

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <i class="fa-solid fa-shield-halved fa-beat"></i> <b>EncryptoDoc</b>
-                </a>
-                @auth
-                {{auth()->user()->name}}
-                    <div class="text-end">
-                    <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2">Logout</a>
-                    </div>
-                @endauth
-            </div>
-        </nav>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <i class="fa-solid fa-shield-halved fa-beat"></i> <b>EncryptoDoc</b>
+        </a>
+        @auth
+        <div class="text-end">
+            <span class="text-light me-3">{{ auth()->user()->username }}</span>
+            <a href="{{ route('logout.perform') }}" class="btn btn-outline-light">Logout</a>
+        </div>
+        @endauth
+    </div>
+</nav>
