75c625cde0d84c34779140aa248f7759adb3a4e6
frncsMonteza <fdmonteza09@gmail.com>
2024-03-05 21:38:36 +0800

    backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,10 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Spatie\Backup\Commands\BackupCommand;
+use Spatie\Backup\Commands\CleanupCommand;
+use Spatie\Backup\Commands\MonitorCommand;
+use Spatie\Backup\Tasks\Backup\BackupJob;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -13,6 +17,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command(BackupCommand::class)->daily()->at('02:00');
+        $schedule->command(CleanupCommand::class)->daily()->at('03:00');
+        $schedule->command(MonitorCommand::class)->daily()->at('04:00');
     }
 
     /**
@@ -20,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
