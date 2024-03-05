b4226451113ed0608a51cf2d0ed0cc0b601d3331
George Vincent Peña <gvbpena@gmail.com>
2024-03-05 22:36:40 +0800

    responsive fix

--- resources/views/layouts/auth-master.blade.php
+++ resources/views/layouts/auth-master.blade.php
@@ -104,7 +104,7 @@
     <section class="background-radial-gradient vh-100">
         <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
             <div class="row gx-lg-5 align-items-center mb-5">
-                <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+                <div class="col-lg-6 mb-5 mb-lg-0 d-none d-lg-block" style="z-index: 10">
                     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image">
                 </div>
 
